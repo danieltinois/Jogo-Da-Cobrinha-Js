e5e09666cca990de519b68d214c7ee0163434027
Daniel <99421761+danieltinois@users.noreply.github.com>
2022-07-16 03:29:37 -0300

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,63 @@
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 9;
+
+const snakeBody = [{ x: 11, y: 11 }];
+
+let pontosTela = document.querySelector(".pontuacao");
+let newSegments = 0;
+
+export function update() {
+  addSegments();
+
+  const inputDirection = getInputDirection();
+
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+
+  newSegments = 0;
+  let placar = snakeBody.length - 1;
+  const tituloPlacar = "Score: ";
+  pontosTela.innerHTML = tituloPlacar + placar;
+}
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return;
+    return position.x === segment.x && position.y === segment.y;
+  });
+}
+
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(getSnakeHead(), {
+    ignoreHead: true,
+  });
+}
